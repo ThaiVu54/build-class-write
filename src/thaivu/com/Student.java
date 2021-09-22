c849a06642181a517ebff206d73eb761f702fb59
ThaiVu54 <thai01654369440@gmail.com>
2021-09-22 08:17:10 +0700

    test student

---
+++ src/thaivu/com/Student.java
@@ -0,0 +1,20 @@
+package thaivu.com;
+
+public class Student {
+    private  String name = "John";
+    private String classes = "C02";
+
+    public Student() {
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setClasses(String classes) {
+        this.classes = classes;
+    }
+    public String toString(){
+        return "name: "+this.name+", class: "+this.classes;
+    }
+}
---
+++ src/thaivu/com/TestStudent.java
@@ -0,0 +1,10 @@
+package thaivu.com;
+
+public class TestStudent {
+    public static void main(String[] args) {
+        Student st1 = new Student();
+        st1.setName("thai");
+        st1.setClasses("C0821H1");
+        System.out.println(st1.toString());
+    }
+}
